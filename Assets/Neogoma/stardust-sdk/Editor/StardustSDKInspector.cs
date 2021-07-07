2e93a88fafb96221541c619576fe292164276669
Heri Andrianjafy <heri@neogoma.com>
2021-07-07 17:19:56 +0800

    Init the project

---
+++ Assets/Neogoma/stardust-sdk/Editor/StardustSDKInspector.cs
@@ -0,0 +1,69 @@
+﻿using com.Neogoma.HoboDream.Network;
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace com.Neogoma.Stardust.API.CustomsEditor
+{
+    [CustomEditor(typeof(StardustSDK))]
+    public class StardustSDKInspector : Editor, IJSonRequestListener
+    {
+        private SerializedProperty apiKey;
+        private string lastApiKey;
+        private string serverVersion;
+
+
+        private void OnEnable()
+        {
+            serverVersion = StardustSDK.SDKVersion;
+            apiKey = serializedObject.FindProperty("ApiKey");
+            lastApiKey = apiKey.stringValue;      
+
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (serverVersion.CompareTo(StardustSDK.SDKVersion) == 0)
+            {
+                EditorGUILayout.HelpBox("SDK Version " + StardustSDK.SDKVersion, MessageType.Info);
+            }
+            else
+            {
+                EditorGUILayout.HelpBox("SDK Version " + StardustSDK.SDKVersion + "\nServer version"+serverVersion+" please update your SDK", MessageType.Warning);
+            }
+
+            if (GUILayout.Button("Open documentation"))
+            {
+                Application.OpenURL("https://neogoma.github.io/stardust-SDK-doc/");
+            }
+
+            //EditorGUILayout.PropertyField();
+            lastApiKey = EditorGUILayout.TextField("ApiKey", lastApiKey);
+
+            if (lastApiKey.CompareTo(apiKey.stringValue) != 0)
+            {
+                apiKey.stringValue = lastApiKey;
+                serializedObject.ApplyModifiedProperties();
+                EditorUtility.SetDirty(target);
+
+
+            }
+
+            if (string.IsNullOrEmpty(lastApiKey))
+            {
+                EditorGUILayout.HelpBox("Please enter an API key (unless you initialize it programatically)", MessageType.Error);
+            }
+           
+        }
+
+        public void RequestSucess(string jsonResult, string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RequestFailed(string error, string key)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
