2fb263ae14bcb8787f24839283263e542e18615e
Heri Andrianjafy <heri@neogoma.com>
2021-07-08 17:11:50 +0800

    Init the repository with content (#1)
    Adding the initial assets and 0.6 plugin

---
+++ Assets/RelocationExample.cs
@@ -0,0 +1,29 @@
+using com.Neogoma.Stardust.API.Relocation;
+using com.Neogoma.Stardust.Datamodel;
+using UnityEngine;
+
+public class RelocationExample : MonoBehaviour
+{
+    public string id;
+
+    public GameObject locateButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Safety here, make sure the button is not enabled until the map download is done
+        locateButton.SetActive(false);
+
+        //Adding the listener
+        MapRelocationManager.Instance.onMapDownloadedSucessfully.AddListener(MapDownloaded);
+
+        //Start downloading the map
+        MapRelocationManager.Instance.GetDataForMap(id);
+    }
+
+    private void MapDownloaded(Session session, GameObject map)
+    {
+        //The map is downloaded we can relocate now
+        locateButton.SetActive(true);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# simple-relocation-example
-simple-relocation-example-doc
+# Tutorial simple relocation
+This repository represents the final result of the [simple relocation tutorial](https://neogoma.github.io/stardust-SDK-doc/#/developer/0_1_empty_project?id=creating-a-scene-for-relocation-with-stardust-sdk) of Stardust SDK
